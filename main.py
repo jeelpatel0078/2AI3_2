5dc7605aeb5fc3fe84fde193486ab6ec4cf07652
jeelpatel0078 <jeelkhandivar06460@gmail.com>
2026-04-05 17:53:17 +0530

    Created a python file and added the depended library

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+import warnings
+warnings.filterwarnings('ignore')
